0e1fa8188281166cd4a4ea5766d4e4147881ec90
Satwik Srivastava <41414390+satwik707@users.noreply.github.com>
2021-10-20 12:18:42 +0530

    Add files via upload

---
+++ Backend/router.js
@@ -0,0 +1,12 @@
+//routes.js
+//initialize express router
+let router = require('express').Router();
+//set default API response
+router.get('/', function(req, res) {
+    res.json({
+        status: 'API Works',
+        message: 'Welcome to FirstRest API'
+    });
+});
+//Export API routes
+module.exports = router;
